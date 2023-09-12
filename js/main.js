424ea53ee42c3523d3b97c699430aadef743c7d5
zakaria <zakaria.fthr@gmail.com>
2023-09-12 21:35:02 +0200

    created banner section and add slider

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// slider banner 
+$('.banner-slider').slick({
+    infinite: true,
+    slidesToScroll: 1,
+    speed: 1000,
+    autoplay : true,
+    nextArrow: $('.fa-arrow-right'),
+    prevArrow: $('.fa-arrow-left'),
+});
